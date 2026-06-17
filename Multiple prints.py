53412bf91cc1d72eb6e624c6ae11e61fd43fd04f
missbug1182013 <hinatahir114@gmail.com>
2026-06-17 20:34:58 +0500

    files

---
+++ Multiple prints.py
@@ -0,0 +1,5 @@
+print("Welcome to the world of programming!")
+print(4)
+print("Hello python \n")
+print("Hello" ,5)
+print("Welcome to" , end="*")
---
+++ Print statement.py
@@ -0,0 +1,13 @@
+# Printing a greeting message
+print("Hello, world")
+
+#Printing my name
+print("My name is Rumaysa.")
+
+#Printing fun facts about myself
+#Printing "I love sculpturing with light clay."
+print("I love sculpturing with light clay.")
+
+#perfoming and printing a calculation
+print("The result of the square root of 49 is:", 7)
+print("The result of 6 + 7 is:", 6 + 7)
