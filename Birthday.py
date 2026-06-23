3548a35a98da2485be71d482941146c41ae6406d
missbug1182013 <hinatahir114@gmail.com>
2026-06-23 21:15:04 +0500

    files

---
+++ Birthday.py
@@ -0,0 +1,16 @@
+print("Welcome to the Birthday Program!\n")
+
+birthday1 = "12-01-2010"
+birthday2 = "25-03-2011"
+birthday3 = "18-07-2010"
+birthday4 = "09-11-2011"
+birthday5 = "30-12-2010"
+
+print("Birthday 1:", birthday1)
+print("Birthday 2:", birthday2)
+print("Birthday 3:", birthday3)
+print("Birthday 4:", birthday4)
+print("Birthday 5:", birthday5)
+
+print("\nStored 5 birthday dates")
+print("Program Finished")
---
+++ String.py
@@ -0,0 +1,9 @@
+text = str(input("Enter a string: "))
+
+revText = text[::-1]
+print("Reverse of given string is:")
+print(revText)
+
+print("Uppercase:", text.upper())
+print("Lowercase:", text.lower())
+print("Length of the string:", len(text))
