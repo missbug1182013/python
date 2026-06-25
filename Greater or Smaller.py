a100866d437db07829a3b2593ee712377e7031eb
missbug1182013 <hinatahir114@gmail.com>
2026-06-25 20:30:39 +0500

    files

---
+++ Greater or Smaller.py
@@ -0,0 +1,8 @@
+i = int(input("Enter a Number : "))
+if ( i < 15):
+    print("i is smaller than 15")
+    print("i'm in if Block")
+else:
+    print("i is grater that 15")
+    print ("i'm in else Block ")
+print("i'm not in if or else Block")
---
+++ Square root.py
@@ -0,0 +1,6 @@
+import math
+
+num = float(input("Enter a number: "))
+sqrt_num = math.sqrt(num)
+
+print("The square root is:", sqrt_num)
